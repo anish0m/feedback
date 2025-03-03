d6ff2c527299e8e718bbac640c2daccf6babd0a5
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-03-03 14:44:13 +0600

    Configuring ModelForm

--- reviews/forms.py
+++ reviews/forms.py
@@ -2,7 +2,25 @@ from django import forms
 
 from .models import Review
 
+
 class ReviewForm(forms.ModelForm):
-  class Meta:
-    model = Review
-    fields = '__all__'
+    class Meta:
+        model = Review
+        fields = "__all__"
+        labels = {
+            "user_name": "Your Name",
+            "review_text": "Your Feedback",
+            "rating": "Your Rating"
+        }
+        error_messages = {
+            "user_name": {
+                "required": "Username cannot be enpty!",
+                "max_length": "Please enter a shorter username."
+            }
+            # "review_text": {
+            #     # 
+            # },
+            # "rating": {
+            #     # 
+            # }
+        }
