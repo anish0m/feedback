8104d2ab0819d834b2df084cf6516bc3bdba381d
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-03-03 14:05:33 +0600

    Saving form-data to database

--- reviews/views.py
+++ reviews/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 
@@ -11,15 +12,21 @@ def review(request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
-            print(form.cleaned_data)
+            review = Review(
+                user_name=form.cleaned_data['user_name'], 
+                review_text=form.cleaned_data['review_text'], 
+                rating=form.cleaned_data['rating'])
+            
+            review.save()
             return HttpResponseRedirect("/thank-you")
-    
+
     else:
         form = ReviewForm()
 
     return render(request, "reviews/review.html", {
         "form": form
     })
+
 
 def thank_you(request):
     return render(request, "reviews/thank_you.html")
